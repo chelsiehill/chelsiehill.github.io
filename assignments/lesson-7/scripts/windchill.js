8cba16557a3ec2d60d77284511d8096e9b22cbf2
chelsiehill <chelsie9@byui.edu>
2019-06-07 13:44:33 -0600

    commit

---
+++ assignments/lesson-7/scripts/windchill.js
@@ -0,0 +1,12 @@
+//input
+let temperature = document.getElementById('temp').innerHTML;
+let windspeed = document.getElementById('windspeed').innerHTML;
+
+//process
+let windchill = 35.74 + 0.6215 * temp - (35.75 * Math.pow(windspeed, 0.16)) + (0.4275 * temp * Math.pow(windspeed, 0.16));
+
+//output
+document.getElementById('windchill').innerHTML = windchill;
+
+
+
