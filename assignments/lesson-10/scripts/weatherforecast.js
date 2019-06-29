0f9fc97686c925ab8c4cf49d07a42c012cef55fa
chelsiehill <chelsie9@byui.edu>
2019-06-29 16:29:48 -0600

    commit

---
+++ assignments/lesson-10/scripts/weatherforecast.js
@@ -0,0 +1,35 @@
+let weatherRequest = new XMLHttpRequest();
+let apiURLstring = 'http://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=78c05017e1ba7edbb699f99a4fb4aec5';
+weatherRequest.open('Get',apiURLstring, true);
+weatherRequest.send();
+
+weatherRequest.onload = function() {
+    let forecastData = JSON.parse(weatherRequest.responseText);
+    console.log(forecastData);
+   
+    var weekday = new Array(7);
+    weekday[0] ="Monday";
+    weekday[1] ="Tuesday";
+    weekday[2] ="Wednesday";
+    weekday[3] ="Thursday";
+    weekday[4] ="Friday";
+    weekday[5] ="Saturday";
+    weekday[6] ="Sunday";
+
+    let count = 1;
+
+    for (let i = 0; i < forecastData.list.length; i++) {
+        if (forecastData.list[i].dt_txt.includes("18:00:00")) {
+            document.getElementById('degreeforecast' +count).innerHTML=forecastData.list[i].main.temp_max;
+            document.getElementById('imgforecast'+count).setAttribute('src', "http://openweathermap.org/img/w/"+forecastData.weather[0] + ".png");
+            document.getElementById('dayforecast' +count).innerHTML=weekday[(new Date().getDay()+count-1)%7];
+            count ++;
+    }
+    if (count==6){
+        break;
+    }
+}
+}
+
+
+
